28b90893bb87493ff33da8f2c4bef2d4dc62c337
Jorge Lagos <jlagos@metrocapital.cl>
2023-03-22 14:21:03 -0300

    Update file: services new token

--- services.py
+++ services.py
@@ -3,7 +3,7 @@ import json
 
 def send_message_wsp(data):
     try:
-        token = 'EAACMBtknV6EBAO5fmBGZCTQrpMxx5iNGz72pnMPZAyV2ZCXLIoZCzPoYaZBMNQz7pka37kyDsTZBZAIVf2XZCwMJUqjPZAZADfT2My4WZAOd6ns5hF0IPkxc9xotejpItvrQ3O0PYBhjFMjsHTRfOgJ1M5awygCeeV4hVu7OUxvCdNh9WluniGdLyEwrvqY5gjzJmcMCZBI3ULef1dJDC8VLbSxkAW9k89vPuJQZD'
+        token = 'EAACMBtknV6EBAINiUghub36TvS3Wt1HhFuZAlTTSOIZBmjzV5H8TSZByH8EzOPdNFcgfHIDlOhAdizEWUstVwnyoEcJMAbeaDhODNIQFK7F0RZBEgUvl1kALVK8n0OSYTFaJoLgNqo0blFU54OlZCHZBe2GrmcltBk56kv0LiEozKfSAjwZCHfWWrppjTHkFGVsWvly5jf5bVqkQIsGYmkUk1fED414nf4ZD'
         api_url = 'https://graph.facebook.com/v15.0/113189058361758/messages'
         headers = {
             'Content-Type': 'application/json',
